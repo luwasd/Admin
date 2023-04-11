1d73e55f70e6fe0e1b0fbab83f737f1557aaac11
luwasd <lmanuel14@gmail.com>
2023-04-10 21:44:26 -0400

    fist commit

---
+++ server/models/person.model.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const personSchema = new mongoose.Schema({
+        firstName: {type: String, required: [true, 'First name is required'], minlength: [2, 'First name must be at least 2 characters long']},
+        lastName: {type: String, required: [true, 'Last name is required'], minlength: [2, 'Last name must be at least 2 characters long']},
+    }, {timestamps: true});
+
+    module.exports.Person = mongoose.model('Person', personSchema);
